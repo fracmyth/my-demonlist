69f481be5de2e8e7669210956ab06cfe9f2af632
fracmyth <156635368+fracmyth@users.noreply.github.com>
2025-08-09 07:49:09 +0000

    August 8th 2025

---
+++ src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<div class="bg-gray-200 h-1000">
+<div class="h-4"></div>
+<div class="flex-col flex items-center">
+    <h1 class="title scale-125 text-4xl bg-gradient-to-r from-red-700 via-red-800 to-black inline-block text-transparent bg-clip-text">Demonlist</h1>
+    <div class="h-4"></div>
+    <div class="space-y-5">
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/-X_uf7PV4cU/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 24px">#1 - Poltergeist</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by AndromedaGMD</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 16</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 16</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 5/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-red-700 font-italic m-2">Insane Demon</div>
+    </section>
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/x-JRfZaOVUA/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 18px">#2 - Paracosm Circles</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by N1xo</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 6</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 7</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 7/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-indigo-800 font-italic m-2">Medium Demon</div>
+    </section>
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/yHv2Hc8Ql94/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 24px">#3 - Zafari</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by Rustam</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 5</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 5</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 8/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-blue-600 font-italic m-2">Easy Demon</div>
+    </section>
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/vKIo0d3a9bA/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 15px">#4 - Platinum Adventure</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by ItsJerry4vn</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 1</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 2</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 6/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-blue-600 font-italic m-2">Easy Demon</div>
+    </section>
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/z89-n-1GStc/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 20px">#5 - The Nightmare</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by Jax</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 1</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 1</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 5/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-blue-600 font-italic m-2">Easy Demon</div>
+    </section>
+    <section class="w-120 rounded h-40 bg-white flex relative justify-start items-center">
+        <img class="h-30 object-cover m-4 rounded" src="https://i.ytimg.com/vi/kZ0wkJXnORQ/maxresdefault.jpg">
+        <div class="levelname absolute top-2 left-65 font-bold m-2" style="font-size: 16px">#6 - The Lightning Road</div>
+        <div class="desc absolute top-10 left-65 font-bold font-italic m-2">by timeless real</div>
+        <div class="desc absolute top-15 left-65 font-bold font-italic m-2">GDDL Tier: 1</div>
+        <div class="desc absolute top-20 left-65 font-bold font-italic m-2">My GDDL Tier: 1</div>
+        <div class="desc absolute top-25 left-65 font-bold font-italic m-2">Enjoyment: 6/10</div>
+        <div class="desc absolute top-30 left-65 font-bold text-blue-600 font-italic m-2">Easy Demon</div>
+    </section>
+    </div>
+</div>
+</div>
+<style>
+    .title {
+        font-family: "Fontdiner Swanky", serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .levelname {
+        font-family: "Montserrat", sans-serif;
+    }
+
+    .desc {
+        font-family: "Montserrat", sans-serif;
+        font-size: 14px;
+    }
+</style>
